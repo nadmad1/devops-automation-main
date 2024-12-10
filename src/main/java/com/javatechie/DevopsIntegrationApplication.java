507279e9d2307ad2235cb85bbb77809792d6ba42
nadmad1 <74018856+nadmad1@users.noreply.github.com>
2024-12-11 02:44:04 +0530

    Update DevopsIntegrationApplication.java

--- src/main/java/com/javatechie/DevopsIntegrationApplication.java
+++ src/main/java/com/javatechie/DevopsIntegrationApplication.java
@@ -12,6 +12,7 @@ public class DevopsIntegrationApplication {
 	@GetMapping
 	public String message(){
 		return "welcome to javatechie";
+		return "welcome NADMAD1";
 	}
 
 	public static void main(String[] args) {
